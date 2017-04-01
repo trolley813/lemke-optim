473fbeedfb64122d1807bcf1bffa8e6f0b70bf96
Bulat F <write2bulat@gmail.com>
2017-04-01 14:57:33 +0300

    Separate Reporter form Solver

--- quadra_new.py
+++ quadra_new.py
@@ -3,11 +3,12 @@ from fractions import Fraction
 
 import solver
 import plotter
+from reporter import Reporter
 
 z = np.array([[2, 6], [1, 7], [8, 5]], dtype=Fraction)
 p = np.array([[4, 3], [6, 4], [1, 0]], dtype=Fraction)
 
-s = solver.Solver(z, p)
+s = solver.Solver(z, p, Reporter)
 s.solve()
 # Plotting
 s.plot(plotter.Plotter)
