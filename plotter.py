de4687b0982fea6886ac996841e99cbd1922f5ea
hyst329 <trolleybus.1329@gmail.com>
2017-05-01 18:05:14 +0300

    plotter is more beautiful now

--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 class Plotter:
     @staticmethod
@@ -10,16 +11,24 @@ class Plotter:
         hull_z = ConvexHull(z)
         hull_p = ConvexHull(p)
         hull_zmp = ConvexHull(zmp)
-        plt.figure()
-        plt.subplot(121)
+        f = plt.figure(figsize=(20,10))
+        a1 = f.add_subplot(121)
         plt.plot(z[:, 0], z[:, 1], 'go')
         for simplex in hull_z.simplices:
             plt.plot(z[simplex, 0], z[simplex, 1], 'g--')
         plt.plot(p[:, 0], p[:, 1], 'bo')
         for simplex in hull_p.simplices:
             plt.plot(p[simplex, 0], p[simplex, 1], 'b--')
-        plt.subplot(122)
+        a2 = f.add_subplot(122)
         plt.plot(zmp[:, 0], zmp[:, 1], 'ro')
         for simplex in hull_zmp.simplices:
             plt.plot(zmp[simplex, 0], zmp[simplex, 1], 'r--')
+        a2.grid(True, ls="--")
+        a1.grid(True, ls="--")
+        a1.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        a1.yaxis.set_major_locator(ticker.MultipleLocator(1))
+        a2.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        a2.yaxis.set_major_locator(ticker.MultipleLocator(1))
+        a1.set_aspect('equal', 'datalim')
+        a2.set_aspect('equal', 'datalim')
         plt.savefig(figurefile)
